cc57e679865c34986ea9992de2935c5ae2d67e2d
dhowell <dhowell@techvalleyhigh.org>
2021-12-17 09:19:02 -0500

    test

--- src/hospitals.java
+++ src/hospitals.java
@@ -1,2 +1,29 @@
+import java.util.Scanner;
 public class hospitals {
+
+    public static void main(String[] args) {
+
+        while (true) {
+            System.out.println("Hospital Directory");
+            System.out.println("");
+            System.out.println("1. Departments");
+            System.out.println("2. Hospital name");
+            System.out.println("3. Hospital address");
+            System.out.println("4. Patient");
+            System.out.println("");
+            System.out.println("Enter directory number:");
+            Scanner input = new Scanner(System.in);
+            int placeholder = input.nextInt();
+            if (placeholder == 1) {
+                //put a method here that shows different things in department
+            } else if (placeholder == 2) {
+                //put a method here
+            } else if (placeholder == 3) {
+                //put a method here
+            } else if (placeholder == 4) {
+                //put a method here
+            }
+        }
+    }
+    public static
 }
