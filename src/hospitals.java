2b7a1af056c6fcd012845838ea0c800147552dea
dhowell0 <95486101+dhowell0@users.noreply.github.com>
2021-12-23 09:29:20 -0500

    had to restart

--- src/hospitals.java
+++ src/hospitals.java
@@ -1,72 +1,30 @@
+//big list of copy and paste
+//Scanner input = new Scanner(System.in);
+//System.out.println("");
+//
+//
+
+
+
 import java.util.Scanner;
 public class hospitals {
 
     public static void main(String[] args) {
+        System.out.println("Directory");
+        System.out.println("");
+        System.out.println("1. List Hospitals");
+        System.out.println("2. Add Hospitals");
+        System.out.println("3. Remove Hospital");
+        System.out.println("4. List Doctors");
+        System.out.println("5. Add Doctors");
+        System.out.println("6. Remove Doctors");
+        System.out.println("7. List Nurses");
+        System.out.println("8. Add Nurses");
+        System.out.println("9. Remove Nurses");
 
-    }
-        class LinkedListHospital {
-            private Node headHospital;
-
-            public void insertHospital(String name) {
-
-                Node add = new Node();
-                add.name = name;
-                add.next = null;
-
-                add.next = headHospital;
-                headHospital = add;
-                return;
-
-            }
-
-            public void removeHospital(String name) {
-
-                if (name == headHospital.name) {
-                    headHospital = headHospital.next;
-                } else {
-                    Node pos = headHospital;
-                    for (int i = 0; name != pos.next.name; i++) {
-                        pos = pos.next;
-                    }
-                    Node temp = null;
-                    temp = pos.next;
-                    pos.next = temp.next;
-                    temp = null;
-                }
-            }
-        }
-        class LinkedListDoctor {
-
-            private Node headDoctor;
-
-            public void insertDoctor(String name) {
-
-                Node add = new Node();
-                add.name = name;
-                add.next = null;
-
-                add.next = headHospital;
-                headHospital = add;
-                return;
-
-            }
-
-            public void removeDoctor(String name) {
-
-                if (name == headDoctor.name) {
-                    headDoctor = headDoctor.next;
-                } else {
-                    Node pos = headDoctor;
-                    for (int i = 0; name != pos.next.name; i++) {
-                        pos = pos.next;
-                    }
-                    Node temp = null;
-                    temp = pos.next;
-                    pos.next = temp.next;
-                    temp = null;
-                }
-            }
+        while (true){
 
         }
     }
+}
 
