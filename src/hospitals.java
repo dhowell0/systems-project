461a67cf95e9327c5bb586150c88d16ff118401c
dhowell0 <95486101+dhowell0@users.noreply.github.com>
2021-12-28 15:29:37 -0500

    started working on arrays

--- src/hospitals.java
+++ src/hospitals.java
@@ -27,38 +27,49 @@ public class hospitals {
             Scanner input = new Scanner(System.in);
             int user = input.nextInt();
 
-            if (user == 1){
-                System.out.println(Arrays.toString(hospitals));
-                System.out.println("");
-                System.out.println("Directory");
-                System.out.println("");
-                System.out.println("1. Add Hospitals");
-                System.out.println("2. Remove Hospital");
-                System.out.println("3. Back");
-                int user2 = input.nextInt();
+            if (user == 1) {
 
-                if (user2 == 1){
-                    String[] h = new String[hospitals.length+1];
-                    System.out.println("name of hospital");
-                    String user3 = input.next();
-                    h[h.length-1]=user3;
-                    hospitals=h;
-                    //note to self need to fix
-                    return;
-                }else if (user2==2){
-                    //this is where remove goes
+                    System.out.println(Arrays.toString(hospitals));
+                    System.out.println("");
+                    System.out.println("Directory");
+                    System.out.println("");
+                    System.out.println("1. Add Hospitals");
+                    System.out.println("2. Remove Hospital");
+                    System.out.println("3. Back");
+                    int user2 = input.nextInt();
+                    while (true) {
+                        if (user2 == 1) {
+                            String[] h = new String[hospitals.length + 1];
+                            System.out.println("name of hospital");
+                            String user3 = input.next();
+                            h[h.length - 1] = user3;
+                            hospitals = h;
+                            //note to self need to fix
+                            break;
+                        } else if (user2 == 2) {
+                            //this is where remove goes
 
 
-
-
-                }else if (user2==3){
-                    //note to self need to fix
-                    break;
-                }
+                        } else if (user2 == 3) {
+                            //note to self need to fix
+                            break;
+                        }
+                    }
             } else {
 
             }
         }
     }
 }
-
+//menu stuff will finish at a later time
+boolean isValid=false;
+while (!isValid){
+        printPrompt()
+        int input=getInput();
+        if(input>0 && input<=3){
+        isValid=true;
+        switch(input){
+        // do stuff
+        }
+        }
+        }
