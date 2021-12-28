4ff1a48944e94b5cc004936209a6ad68752826b8
dhowell0 <95486101+dhowell0@users.noreply.github.com>
2021-12-28 13:56:40 -0500

    started working on arrays

--- src/hospitals.java
+++ src/hospitals.java
@@ -5,7 +5,7 @@
 //
 
 
-
+import java.util.Arrays;
 import java.util.Scanner;
 public class hospitals {
 
@@ -13,17 +13,51 @@ public class hospitals {
         System.out.println("Directory");
         System.out.println("");
         System.out.println("1. List Hospitals");
-        System.out.println("2. Add Hospitals");
-        System.out.println("3. Remove Hospital");
-        System.out.println("4. List Doctors");
-        System.out.println("5. Add Doctors");
-        System.out.println("6. Remove Doctors");
-        System.out.println("7. List Nurses");
-        System.out.println("8. Add Nurses");
-        System.out.println("9. Remove Nurses");
+        //System.out.println("2. Add Hospitals");
+        //System.out.println("3. Remove Hospital");
+        //System.out.println("4. List Doctors");
+        //System.out.println("5. Add Doctors");
+        //System.out.println("6. Remove Doctors");
+        //System.out.println("7. List Nurses");
+        //System.out.println("8. Add Nurses");
+        //System.out.println("9. Remove Nurses");
+        String[] hospitals = new String[0];
 
         while (true){
+            Scanner input = new Scanner(System.in);
+            int user = input.nextInt();
 
+            if (user == 1){
+                System.out.println(Arrays.toString(hospitals));
+                System.out.println("");
+                System.out.println("Directory");
+                System.out.println("");
+                System.out.println("1. Add Hospitals");
+                System.out.println("2. Remove Hospital");
+                System.out.println("3. Back");
+                int user2 = input.nextInt();
+
+                if (user2 == 1){
+                    String[] h = new String[hospitals.length+1];
+                    System.out.println("name of hospital");
+                    String user3 = input.next();
+                    h[h.length-1]=user3;
+                    hospitals=h;
+                    //note to self need to fix
+                    return;
+                }else if (user2==2){
+                    //this is where remove goes
+
+
+
+
+                }else if (user2==3){
+                    //note to self need to fix
+                    break;
+                }
+            } else {
+
+            }
         }
     }
 }
